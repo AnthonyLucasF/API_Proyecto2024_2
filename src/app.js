36dcff97b62562a402ea2baf9f88171a5eb9d1d5
AnthonyLucasF <anthony.lucasfloreano@gmail.com>
2024-11-26 21:26:09 -0500

    API para Proyecto

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import jwt from 'jsonwebtoken';
-import clientesRoutes from './routes/clientes.routes.js';
+import materiaprimaRoutes from './routes/materiaprima.routes.js';
 import usuariosRoutes from './routes/usuarios.routes.js';
-import productosRoutes from './routes/productos.router.js';
-import pedidosRoutes from './routes/pedidos.route.js';
-import pedidosDetalleRoutes from './routes/pedidos_detalle.route.js';
+import corteRoutes from './routes/corte.routes.js';
+import pesoRoutes from './routes/peso.routes.js';
+import tallaRoutes from './routes/talla.routes.js';
+import proveedorRoutes from './routes/proveedor.routes.js';
+import maquinaRoutes from './routes/maquina.routes.js';
+import procesoRoutes from './routes/proceso.routes.js';
+import empaqueRoutes from './routes/empaque.routes.js';
+import { fileURLToPath } from 'url'; // Importar fileURLToPath
+import path from 'path'; // Importar path
+import cors from 'cors'; // Importar cors
+
+const app = express();
+app.use(express.json()); // Para que interprete los objetos json
 
 // Definir módulo de ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const app = express();
 
 // Configuración de CORS
 const corsOptions = {
@@ -21,21 +26,18 @@ const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
 };
-app.use(cors(corsOptions));
-
-// Middleware para interpretar objetos JSON y formularios
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Servir archivos estáticos
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use(cors(corsOptions)); // Aquí ahora funciona correctamente
 
 // Rutas
-app.use('/api', clientesRoutes);
+app.use('/api', materiaprimaRoutes);
 app.use('/api', usuariosRoutes);
-app.use('/api', productosRoutes);
-app.use('/api', pedidosRoutes);
-app.use('/api', pedidosDetalleRoutes);
+app.use('/api', corteRoutes);
+app.use('/api', pesoRoutes);
+app.use('/api', tallaRoutes);
+app.use('/api', proveedorRoutes);
+app.use('/api', maquinaRoutes);
+app.use('/api', procesoRoutes);
+app.use('/api', empaqueRoutes);
 
 // Ruta de prueba para API
 app.get("/api", (req, res) => {
@@ -58,33 +60,6 @@ app.get("/api/login", (req, res) => {
         });
     });
 });
-
-// Ruta protegida para crear posts, requiere token JWT
-app.post("/api/posts", verifyToken, (req, res) => {
-    jwt.verify(req.token, 'secretkey', (error, authData) => {
-        if (error) {
-            res.sendStatus(403);
-        } else {
-            res.json({
-                mensaje: "Post fue creado",
-                authData: authData
-            });
-        }
-    });
-});
-
-// Middleware para verificar el token JWT
-function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
-
-    if (typeof bearerHeader !== 'undefined') {
-        const bearerToken = bearerHeader.split(" ")[1];
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-}
 
 // Manejo de rutas no encontradas
 app.use((req, res, next) => {
