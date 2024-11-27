83b985c8ed11e8adb94e075fb44b2c11f34115aa
AnthonyLucasF <anthony.lucasfloreano@upse.edu.ec>
2024-11-27 11:55:54 -0500

    Actualización probablemente innecesaria xd

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ import { config } from "dotenv";
 config()
 
 export const BD_HOST=process.env.BD_HOST || localhost
-export const BD_DATABASE=process.env.BD_DATABASE || baseproyecto20242
+/* export const BD_DATABASE=process.env.BD_DATABASE || baseproyecto20242 */
+export const BD_DATABASE=process.env.BD_DATABASE || bwzwfarfe5g3jjz6l07q
 export const DB_USER=process.env.DB_USER || root
 export const DB_PASSWORD=process.env.DB_PASSWORD || ''
 export const DB_PORT=process.env.DB_PORT || 3306
